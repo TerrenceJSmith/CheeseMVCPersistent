52089998a43b39615a668137c8d3f82470aaf659
TerrenceJSmith <34356741+TerrenceJSmith@users.noreply.github.com>
2018-03-29 02:52:18 -0400

    Update CheeseDbContextModelSnapshot.cs

--- src/CheeseMVC/Migrations/CheeseDbContextModelSnapshot.cs
+++ src/CheeseMVC/Migrations/CheeseDbContextModelSnapshot.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
@@ -13,7 +13,7 @@ namespace CheeseMVC.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
             modelBuilder
-                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("ProductVersion", "1.1.2")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("CheeseMVC.Models.Cheese", b =>
@@ -21,15 +21,75 @@ namespace CheeseMVC.Migrations
                     b.Property<int>("ID")
                         .ValueGeneratedOnAdd();
 
+                    b.Property<int>("CategoryID");
+
                     b.Property<string>("Description");
 
                     b.Property<string>("Name");
 
-                    b.Property<int>("Type");
+                    b.HasKey("ID");
+
+                    b.HasIndex("CategoryID");
+
+                    b.ToTable("Cheeses");
+                });
+
+            modelBuilder.Entity("CheeseMVC.Models.CheeseCategory", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
 
                     b.HasKey("ID");
 
-                    b.ToTable("Cheeses");
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("CheeseMVC.Models.CheeseMenu", b =>
+                {
+                    b.Property<int>("CheeseID");
+
+                    b.Property<int>("MenuID");
+
+                    b.HasKey("CheeseID", "MenuID");
+
+                    b.HasIndex("MenuID");
+
+                    b.ToTable("CheeseMenus");
+                });
+
+            modelBuilder.Entity("CheeseMVC.Models.Menu", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Menus");
+                });
+
+            modelBuilder.Entity("CheeseMVC.Models.Cheese", b =>
+                {
+                    b.HasOne("CheeseMVC.Models.CheeseCategory", "Category")
+                        .WithMany("Cheeses")
+                        .HasForeignKey("CategoryID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("CheeseMVC.Models.CheeseMenu", b =>
+                {
+                    b.HasOne("CheeseMVC.Models.Cheese", "Cheese")
+                        .WithMany("CheeseMenus")
+                        .HasForeignKey("CheeseID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("CheeseMVC.Models.Menu", "Menu")
+                        .WithMany("CheeseMenus")
+                        .HasForeignKey("MenuID")
+                        .OnDelete(DeleteBehavior.Cascade);
                 });
         }
     }
